caf45f7c43829cd54781d7254f77773523d397b1
Ilia <istudyatuni@gmail.com>
2022-03-14 14:22:34 +0300

    fix(deviantart): check for empty urls list

--- sites/deviantart.py
+++ sites/deviantart.py
@@ -303,9 +303,12 @@ async def download(url: list[str] | str, data_folder: str) -> None:
 		await find_and_download_art(service, save_folder, artist, url, parsed['name'])
 
 async def download_list(urls: list[str], data_folder: str):
+	if len(urls) == 0:
+		return
+
 	service = DAService()
 
-	print('Saving to folder', data_folder)
+	print('\nSaving to folder', data_folder)
 
 	# ['artist1', ...]
 	mapping_all: list[str] = []
