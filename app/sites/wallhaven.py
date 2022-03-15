3cf0ded5ded27de9a312505a6cec420f4b23697c
Ilia <istudyatuni@gmail.com>
2022-03-15 13:05:37 +0300

    feat: use wallhaven api and save tags to file name

--- app/sites/wallhaven.py
+++ app/sites/wallhaven.py
@@ -4,9 +4,9 @@ import aiofiles
 import aiohttp
 import os.path
 
-from app.utils import mkdir
+from app.utils import filename_normalize, mkdir
 
-IMAGE_URL = 'https://w.wallhaven.cc/full/{prefix}/wallhaven-{id}'
+API_URL = 'https://wallhaven.cc/api/v1/w/'
 
 def parse_link(url: str):
 	parsed = urlparse(url)
@@ -15,18 +15,14 @@ def parse_link(url: str):
 	if parsed.netloc == 'wallhaven.cc':
 		# strip 'w'
 		path.pop(0)
-	# id: abcdef, prefix: ab
-	return { 'id': path[0], 'prefix': path[0][0:2] }
+
+	return { 'id': path[0] }
 
 async def fetch_image(session: aiohttp.ClientSession, url: str, name: str, folder: str):
 	async with session.get(url) as response:
-		if response.status == 404:
-			print('Not found', name)
-			return False
 		async with aiofiles.open(os.path.join(folder, name), 'wb') as file:
 			await file.write(await response.read())
 			print('Download:', name)
-			return True
 
 async def download(urls_to_download: list[str] | str, data_folder: str):
 	urls = urls_to_download if isinstance(urls_to_download, list) else [urls_to_download]
@@ -35,16 +31,19 @@ async def download(urls_to_download: list[str] | str, data_folder: str):
 	async with aiohttp.ClientSession() as session:
 		for url in urls:
 			parsed = parse_link(url)
-			existing = glob(f'{data_folder}/{parsed["id"]}.*')
+			existing = glob(f'{data_folder}/{parsed["id"]} - *.*')
 			if len(existing) == 1:
 				print('Skip existing:', parsed['id'])
 				continue
 
-			full_url = IMAGE_URL.format(prefix=parsed['prefix'], id=parsed['id'])
-			name = parsed['id']
-			exts = ['.jpg', '.png']
+			async with session.get(API_URL + parsed['id']) as response:
+				data = (await response.json())['data']
 
-			for ext in exts:
-				status = await fetch_image(session, full_url + ext, name + ext, data_folder)
-				if status:
-					break
+			full_url = data['path']
+			name = (
+				data['id']
+				+ ' - '
+				+ ', '.join(map(lambda tag: tag['name'], data['tags']))
+			)
+			name = filename_normalize(name) + os.path.splitext(full_url)[1]
+			await fetch_image(session, full_url, name, data_folder)
--- app/utils.py
+++ app/utils.py
@@ -6,4 +6,6 @@ mkdir = partial(makedirs, exist_ok=True)
 print_inline = partial(print, end='\r', flush=True)
 
 def filename_normalize(filename: str):
-	return re.sub(r'[<>:"\\\/|?*]', '_', filename)
+	# max filename size is 255
+	# minus 4 for dot and file extension
+	return re.sub(r'[<>:"\\\/|?*]', '_', filename)[:251]
