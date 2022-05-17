fb11df7272e0c5b17e4b75e8189b75d43e98c594
Ilia <istudyatuni@gmail.com>
2022-05-17 12:20:33 +0300

    refactor: move db logic to class

--- art_dl/cache.py
+++ art_dl/cache.py
@@ -1,55 +1,27 @@
-from json import dumps, loads
 from typing import Any
-import sqlite3 as sl
+
+from art_dl.utils.db import DB
 
 CACHE_DB = '.cache.db'
-
-INIT_QUERY = '''CREATE TABLE IF NOT EXISTS cache (
-	key TEXT NOT NULL PRIMARY KEY,
-	value TEXT
-)'''
-DELETE_QUERY = '''DELETE FROM cache WHERE key = :key'''
-INSERT_QUERY = '''INSERT OR IGNORE INTO cache (key, value) VALUES (:key, :value)'''
-SELECT_QUERY = '''SELECT value FROM cache WHERE key = :key'''
 
 
 class Cache:
 
 	def __init__(self) -> None:
-		self.conn = sl.connect(CACHE_DB)
-		self.conn.row_factory = sl.Row
-		self.cursor = self.conn.cursor()
-
-		self.cursor.executescript(INIT_QUERY)
-		self.conn.commit()
+		self.db = DB(CACHE_DB, 'cache')
 
 	@staticmethod
 	def _key(slug: str | None, key: str):
 		return key if slug is None else slug + ':' + key
 
 	def insert(self, slug: str | None, key: str, value: str | Any, *, as_json=False):
-		# if not as json value should be string
-		if as_json is False and not isinstance(value, str):
-			raise Exception('Invalid value type')
-
-		self.cursor.execute(
-			INSERT_QUERY, {
-				'key': self._key(slug, key),
-				'value': dumps(value) if as_json else value
-			}
-		)
-		self.conn.commit()
+		self.db.insert(self._key(slug, key), value, as_json=as_json)
 
 	def select(self, slug: str | None, key: str, *, as_json=False):
-		res = self.cursor.execute(SELECT_QUERY, (self._key(slug, key), )).fetchone()
-		if res is None:
-			return res
-		value = res['value']
-		return loads(value) if as_json else value
+		return self.db.select(self._key(slug, key), as_json=as_json)
 
 	def delete(self, slug: str | None, key: str):
-		self.cursor.execute(DELETE_QUERY, (self._key(slug, key), ))
-		self.conn.commit()
+		self.db.delete(self._key(slug, key))
 
 
 cache = Cache()
---
+++ art_dl/utils/db.py
@@ -0,0 +1,59 @@
+from json import dumps, loads
+from typing import Any
+import sqlite3 as sql
+
+
+class Queries:
+	init = '''CREATE TABLE IF NOT EXISTS {table} (
+		key TEXT NOT NULL PRIMARY KEY,
+		value TEXT
+	)'''
+	insert = '''INSERT OR IGNORE INTO {table} (key, value) VALUES (:key, :value)'''
+	select = '''SELECT value FROM {table} WHERE key = :key'''
+	delete = '''DELETE FROM {table} WHERE key = :key'''
+
+	def __init__(self, table: str) -> None:
+		for q in ['init', 'insert', 'select', 'delete']:
+			self.__setattr__(q, self.__getattribute__(q).format(table=table))
+
+
+class DB:
+
+	def __init__(self, db_name: str, table: str) -> None:
+		self.db_name = db_name
+		self.queries = Queries(table)
+		self.connect()
+
+	def connect(self):
+		self.conn = sql.connect(self.db_name)
+		self.conn.row_factory = sql.Row
+		self.cursor = self.conn.cursor()
+
+		self.cursor.executescript(self.queries.init)
+		self.conn.commit()
+
+	def insert(self, key: str, value: str | Any, *, as_json=False):
+		# if not as json value should be a string
+		if as_json is False and not isinstance(value, str):
+			raise TypeError('Value should be a string')
+
+		self.cursor.execute(
+			self.queries.insert, {
+				'key': key,
+				'value': dumps(value) if as_json else value,
+			}
+		)
+		self.conn.commit()
+
+	def select(self, key: str, *, as_json=False):
+		res = self.cursor.execute(self.queries.select, {
+			'key': key
+		}).fetchone()
+		if res is None:
+			return res
+		value = res['value']
+		return loads(value) if as_json else value
+
+	def delete(self, key: str):
+		self.cursor.execute(self.queries.delete, { 'key': key })
+		self.conn.commit()
