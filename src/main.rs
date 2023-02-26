10b8b6e83a2524b4e0380a2aaa519d3094486d77
Ilia <istudyatuni@gmail.com>
2023-02-26 14:23:06 +0300

    feat: read urls from file

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 #![feature(return_position_impl_trait_in_trait)]
 #![feature(type_alias_impl_trait)]
 
+use std::{env::args, fs::read_to_string};
+
 use extractor::common::ExtractorOptions;
 pub use model::error::{CrateError, Result};
 
@@ -14,7 +16,12 @@ mod utils;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let urls = vec!["https://imgur.com"];
+    let a: Vec<_> = args().skip(1).collect();
+    if a.len() == 0 {
+        return Err(CrateError::plain("please provide path to file with urls"));
+    }
+    let urls = read_to_string(&a[0])?;
+    let urls: Vec<_> = urls.lines().map(|s| s.trim()).collect();
     let config = ExtractorOptions::new("test");
     extractor::download_urls(urls, &config).await
 }
